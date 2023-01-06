43f280efd66edeabee1ac33df28761fa4076af45
haukurlogi <haukurlogi2008@gmail.com>
2023-01-06 23:27:34 +0000

    whoops forgot to add comments

--- random-float.c
+++ random-float.c
@@ -3,7 +3,9 @@
 #include <time.h>
 
 int main() {
+    // Starting random number generator
     srand((unsigned) time(NULL));
 
+    // Displays random float
     printf("%f\n", ((float)rand() / (float)(RAND_MAX)));
 }
